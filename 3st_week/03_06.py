d66e9b96c95313ab6a44e5c882064d449c85e401
Jimin15 <sujean07015@gmail.com>
2025-09-17 16:28:34 +0900

    스택

---
+++ 3st_week/03_06.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, value):
+        new_head = Node(value)
+        new_head.next = self.head
+        self.head = new_head
+        return
+
+    # pop 기능 구현
+    def pop(self):
+        if self.is_empty():
+            return "stack is Empty"
+
+        delete_head = self.head
+        self.head = self.head.next
+        return
+
+    def peek(self):
+        if self.is_empty():
+            return "stack is Empty"
+        return self.head.data
+
+    # isEmpty 기능 구현
+    def is_empty(self):
+        return self.head is None
+
+stack = Stack()
+stack.push(4)
+print(stack.head.data)
+
+stack.push(3)
+print(stack.head.data)
+
+stack.push(5)
+print(stack.peek())
+
+stack.pop()
+print(stack.peek())
